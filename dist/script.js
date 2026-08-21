5550040ccca3e2719d9b6344b27dcc8aca0fe9a3
Isabelly Franklin <isafranklin883@gmail.com>
2026-08-21 00:43:40 -0300

    Projeto em andamento

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,10 @@
-let nome = "isabelly";
-let idade = 21;
+const navItems = document.querySelectorAll(".navbar-list li");
+const paginaAtual = window.location.pathname.split("/").pop();
+navItems.forEach((item) => {
+    const link = item.querySelector("a");
+    if (link && link.getAttribute("href")?.includes(paginaAtual ?? "")) {
+        item.classList.add("active");
+    }
+});
 export {};
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,2 +1,10 @@
-let nome:string = "isabelly"
-let idade:number = 21
+const navItems = document.querySelectorAll<HTMLLIElement>(".navbar-list li");
+
+const paginaAtual = window.location.pathname.split("/").pop();
+
+navItems.forEach((item) => {
+  const link = item.querySelector("a");
+  if (link && link.getAttribute("href")?.includes(paginaAtual ?? "")) {
+    item.classList.add("active");
+  }
+});
