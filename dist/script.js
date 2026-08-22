c7ccc56606b4161826fccabb368e2d5e51520e9a
Isabelly Franklin <isafranklin883@gmail.com>
2026-08-22 01:54:48 -0300

    Codigo typeScript em pagina medica completo.

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,5 @@
 "use strict";
+// ÁREA MEDICA
 function salvarMedico() {
     const listaTabelaMedico = document.getElementById("medicos-cadastrado");
     const nomeMedico = document.getElementById("nome-medico");
@@ -13,8 +14,52 @@ function salvarMedico() {
     <td>${especialidadeMedico.value}</td>
     <td>${telMedico.value}</td>
     <td>${emailMedico.value}</td>
-    <td><button class="btn-excluir"><img src="../img/icon-trash.svg" alt="Icone excluir"></button></td>
+    <td> 
+    <button
+     class="btn-excluir"
+     onclick="this.closest('tr').remove()"
+     ><img src="../img/icon-trash.svg" alt="Icone excluir">
+     </button>
+     </td>
   `;
+    atualizarTotalMedicos();
     listaTabelaMedico.appendChild(novaLinha);
+    // salva o médico no localStorage
+    const novoMedico = {
+        nome: nomeMedico.value,
+        crm: crmMedico.value,
+        especialidade: especialidadeMedico.value,
+        telefone: telMedico.value,
+        email: emailMedico.value
+    };
+    const medicosSalvos = localStorage.getItem("medicos");
+    const listaMedicos = medicosSalvos ? JSON.parse(medicosSalvos) : [];
+    listaMedicos.push(novoMedico);
+    localStorage.setItem("medicos", JSON.stringify(listaMedicos));
+}
+// REMOVER MEDICO
+function removerMedico() {
+    const listaTabelaMedico = document.getElementById("medicos-cadastrado");
+    const nomeMedico = document.getElementById("nome-medico");
+    const crmMedico = document.getElementById("crm");
+    const especialidadeMedico = document.getElementById("especialidade-medico");
+    const telMedico = document.getElementById("telefone-medico");
+    const emailMedico = document.getElementById("email-medico");
+    nomeMedico.value = "";
+    crmMedico.value = "";
+    especialidadeMedico.value = "";
+    telMedico.value = "";
+    emailMedico.value = "";
+}
+// ATUALIZAR TOTAL DE MEDICO NA TELA INICIAL
+function atualizarTotalMedicos() {
+    const totalAtual = document.querySelectorAll("#medicos-cadastrado tr").length;
+    const totalSpanMedico = document.getElementById("total-de-medicos-cadastrados");
+    if (totalSpanMedico) {
+        totalSpanMedico.textContent = totalAtual.toString();
+    }
+    localStorage.setItem("total-medicos", totalAtual.toString());
+}
+function salvaConsulta() {
 }
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,4 @@
+// ÁREA MEDICA
 function salvarMedico():void {
 
   const listaTabelaMedico = document.getElementById("medicos-cadastrado") as HTMLTableSectionElement;
@@ -14,8 +15,63 @@ function salvarMedico():void {
     <td>${especialidadeMedico.value}</td>
     <td>${telMedico.value}</td>
     <td>${emailMedico.value}</td>
-    <td><button class="btn-excluir"><img src="../img/icon-trash.svg" alt="Icone excluir"></button></td>
+    <td> 
+    <button
+     class="btn-excluir"
+     onclick="this.closest('tr').remove()"
+     ><img src="../img/icon-trash.svg" alt="Icone excluir">
+     </button>
+     </td>
   `;
 
+  atualizarTotalMedicos();
+
   listaTabelaMedico.appendChild(novaLinha);
+
+  // salva o médico no localStorage
+  const novoMedico = {
+    nome: nomeMedico.value,
+    crm: crmMedico.value,
+    especialidade: especialidadeMedico.value,
+    telefone: telMedico.value,
+    email: emailMedico.value
+  };
+
+  const medicosSalvos = localStorage.getItem("medicos");
+  const listaMedicos = medicosSalvos ? JSON.parse(medicosSalvos) : [];
+  listaMedicos.push(novoMedico);
+  localStorage.setItem("medicos", JSON.stringify(listaMedicos));
+}
+
+// REMOVER MEDICO
+function removerMedico():void {
+
+  const listaTabelaMedico = document.getElementById("medicos-cadastrado") as HTMLTableSectionElement;
+  const nomeMedico = document.getElementById("nome-medico") as HTMLInputElement;
+  const crmMedico = document.getElementById("crm") as HTMLInputElement;
+  const especialidadeMedico = document.getElementById("especialidade-medico") as HTMLSelectElement;
+  const telMedico = document.getElementById("telefone-medico") as HTMLInputElement;
+  const emailMedico = document.getElementById("email-medico") as HTMLInputElement;
+
+  nomeMedico.value = "";
+  crmMedico.value = "";
+  especialidadeMedico.value = "";
+  telMedico.value = "";
+  emailMedico.value = "";
+}
+
+// ATUALIZAR TOTAL DE MEDICO NA TELA INICIAL
+function atualizarTotalMedicos(): void {
+  const totalAtual = document.querySelectorAll("#medicos-cadastrado tr").length;
+
+  const totalSpanMedico = document.getElementById("total-de-medicos-cadastrados") as HTMLSpanElement;
+  if (totalSpanMedico) {
+    totalSpanMedico.textContent = totalAtual.toString();
+  }
+
+  localStorage.setItem("total-medicos", totalAtual.toString());
+}
+
+function salvaConsulta(){
+
 }
