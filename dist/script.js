efac7eb9431a0692221945befa1f8fe4aa28ceb7
Isabelly Franklin <isafranklin883@gmail.com>
2026-08-22 00:19:02 -0300

    Foi adicionado codigo typeScript na parte medica, sem localStorage

--- dist/script.js
+++ dist/script.js
@@ -1,10 +1,20 @@
-const navItems = document.querySelectorAll(".navbar-list li");
-const paginaAtual = window.location.pathname.split("/").pop();
-navItems.forEach((item) => {
-    const link = item.querySelector("a");
-    if (link && link.getAttribute("href")?.includes(paginaAtual ?? "")) {
-        item.classList.add("active");
-    }
-});
-export {};
+"use strict";
+function salvarMedico() {
+    const listaTabelaMedico = document.getElementById("medicos-cadastrado");
+    const nomeMedico = document.getElementById("nome-medico");
+    const crmMedico = document.getElementById("crm");
+    const especialidadeMedico = document.getElementById("especialidade-medico");
+    const telMedico = document.getElementById("telefone-medico");
+    const emailMedico = document.getElementById("email-medico");
+    const novaLinha = document.createElement("tr");
+    novaLinha.innerHTML = `
+    <td>${nomeMedico.value}</td>
+    <td>${crmMedico.value}</td>
+    <td>${especialidadeMedico.value}</td>
+    <td>${telMedico.value}</td>
+    <td>${emailMedico.value}</td>
+    <td><button class="btn-excluir"><img src="../img/icon-trash.svg" alt="Icone excluir"></button></td>
+  `;
+    listaTabelaMedico.appendChild(novaLinha);
+}
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,21 @@
-const navItems = document.querySelectorAll<HTMLLIElement>(".navbar-list li");
+function salvarMedico():void {
 
-const paginaAtual = window.location.pathname.split("/").pop();
+  const listaTabelaMedico = document.getElementById("medicos-cadastrado") as HTMLTableSectionElement;
+  const nomeMedico = document.getElementById("nome-medico") as HTMLInputElement;
+  const crmMedico = document.getElementById("crm") as HTMLInputElement;
+  const especialidadeMedico = document.getElementById("especialidade-medico") as HTMLSelectElement;
+  const telMedico = document.getElementById("telefone-medico") as HTMLInputElement;
+  const emailMedico = document.getElementById("email-medico") as HTMLInputElement;
 
-navItems.forEach((item) => {
-  const link = item.querySelector("a");
-  if (link && link.getAttribute("href")?.includes(paginaAtual ?? "")) {
-    item.classList.add("active");
-  }
-});
+  const novaLinha = document.createElement("tr");
+  novaLinha.innerHTML = `
+    <td>${nomeMedico.value}</td>
+    <td>${crmMedico.value}</td>
+    <td>${especialidadeMedico.value}</td>
+    <td>${telMedico.value}</td>
+    <td>${emailMedico.value}</td>
+    <td><button class="btn-excluir"><img src="../img/icon-trash.svg" alt="Icone excluir"></button></td>
+  `;
+
+  listaTabelaMedico.appendChild(novaLinha);
+}
